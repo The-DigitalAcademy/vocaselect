19b319392d0351925fb3019a915d33916602eb7d
Khomotso Maincanye <khomomaincanye@gmail.com>
2023-07-19 14:32:17 +0200

    initial commit

---
+++ FRONTEND/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HamburgerComponent } from './components/hamburger/hamburger.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { SubjectsComponent } from './Pages/subjects/subjects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HamburgerComponent,
+    LandingPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    SubjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
