fe49642b3fd51c5930fd10d5ff854508295ae07b
https://github.com/github-NandiNotha <nandieh99@gmail.com>
2023-07-21 11:25:28 +0200

    landing page code

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
 import { LoginComponent } from './Pages/login/login.component';
 
 const routes: Routes = [
- { path: 'login', component:LoginComponent}
+ { path: 'login', component:LoginComponent},
+ { path:'landing', component: LandingPageComponent}
 ];
 
 @NgModule({
