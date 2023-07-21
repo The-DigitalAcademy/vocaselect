25ab4e57a8c6b1b403225b7d5d7b635cff92834d
DAMAC194 <damac194@DA-MAC94s-MacBook-Air.local>
2023-07-21 11:22:18 +0200

    updated CRUD

--- BACKEND/routes/user.routes.js
+++ BACKEND/routes/user.routes.js
@@ -9,11 +9,13 @@ const router = express.Router()
 //signup endpoint
 //passing the middleware function to the signup
 router.post('/signup', userAuth.saveUser, signup)
+// app.use('/api/users', userRoutes)
+
 
 //login route
 router.post('/login', login )
 
 // Route to get all users
-router.get("/users", userController.getUsers);
+router.get("/", userController.getUsers);
 
 module.exports = router
