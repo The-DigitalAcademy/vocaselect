46f9d2782454de11d8eb60bb712e0afc122862d9
Khomotso Maincanye <khomomaincanye@gmail.com>
2023-07-24 13:05:11 +0200

    second push

--- FRONTEND/src/app/Pages/register/register.component.ts
+++ FRONTEND/src/app/Pages/register/register.component.ts
@@ -10,10 +10,14 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class RegisterComponent implements OnInit {
 
   form: any = {
-    username: null,
+    firstName: null,
+    lastName:null,
     email: null,
+    dateofBirth:null,
+    cityorTown:null,
+    studentGrade:null,
     password: null,
-
+  confirmpassword:null
   };
   isSuccessful = false;
   isSignUpFailed = false;
--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ import { RegisterComponent } from './Pages/register/register.component';
 const routes: Routes = [
  { path: 'login', component:LoginComponent},
  { path: 'register', component:RegisterComponent},
-,
  { path:'landing', component: LandingPageComponent},
  { path: 'subjects', component: SubjectsComponent},
  { path: 'dream-job', component: DreamJobComponent}
