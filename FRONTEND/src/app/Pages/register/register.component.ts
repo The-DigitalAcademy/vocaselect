9e640e2cc45cf10f2068a624e54b5bb64f91a04d
enikma-mod <sirmodika@gmmail.com>
2023-07-26 01:34:50 +0200

    dd

--- FRONTEND/src/app/Pages/register/register.component.ts
+++ FRONTEND/src/app/Pages/register/register.component.ts
@@ -23,35 +23,56 @@ export class RegisterComponent implements OnInit {
   };
 isSignUpFailed: any;
 errorMessage: any;
+  isSuccessful: boolean | undefined;
 
 
   constructor(private userService: UserService, private router: Router) {}
 
-  signup() {
-    this.userService.signup(this.form).subscribe(
-      (response) => {
-        // Handle successful registration response if needed
-        console.log('Registration successful:', response);
+  // signup() {
+  //   this.userService.signup(this.form).subscribe(
+  //     (response) => {
+  //       // Handle successful registration response if needed
+  //       console.log('Registration successful:', response);
 
-        // Show success alert using SweetAlert
-        // Swal.fire({
-        //   icon: 'success',
-        //   title: 'Registration Successful!',
-        //   text: 'You have been successfully registered.',
-        // });
+  //       // Show success alert using SweetAlert
+  //       // Swal.fire({
+  //       //   icon: 'success',
+  //       //   title: 'Registration Successful!',
+  //       //   text: 'You have been successfully registered.',
+  //       // });
         
-      },
-      (error) => {
-        // Handle registration error if needed
-        console.error('Registration error:', error);
-      }
-    );
-  }
+  //     },
+  //     (error) => {
+  //       // Handle registration error if needed
+  //       console.error('Registration error:', error);
+  //     }
+  //   );
+
+   
+  // }
 
   ngOnInit(): void {
   }
 
-  
+  //This code appears to be implementing an onSubmit method for a registration functionality in an Angular application. 
+  //The purpose of this code is to handle the form submission when a user tries to register or sign up.
+  onSubmit(): void {
+    const { name, surname, email, dob, city, studentgrade } = this.form;
+
+    this.userService.signup(this.form).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+        
+      }
+    });
+  }
 }
 
 
