9f02461fb16edf7644287f1e868edc141fcfda8b
https://github.com/github-NandiNotha <nandieh99@gmail.com>
2023-07-25 11:05:23 +0200

    changes

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SubjectsComponent } from './Pages/subjects/subjects.component';
 import { WelcomeQuizComponent } from './Pages/welcome-quiz/welcome-quiz.component';
 import { DreamJobComponent } from './Pages/dream-job/dream-job.component';
 import { CommonModule } from '@angular/common';
+import { FillCareerComponent } from './Pages/fill-career/fill-career.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CommonModule } from '@angular/common';
     RegisterComponent,
     SubjectsComponent,
     WelcomeQuizComponent,
-    DreamJobComponent
+    DreamJobComponent,
+    FillCareerComponent
   ],
   imports: [
     BrowserModule,
