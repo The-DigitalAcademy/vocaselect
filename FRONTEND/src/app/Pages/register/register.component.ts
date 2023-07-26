91ce5463297c74ad6cb135d96034bb26d638150d
neo2621 <neomakhubo25@gmail.com>
2023-07-26 08:18:10 +0200

    dream job update

--- FRONTEND/src/app/Pages/register/register.component.ts
+++ FRONTEND/src/app/Pages/register/register.component.ts
@@ -36,7 +36,7 @@ export class RegisterComponent implements OnInit {
         console.log(data);
         this.isSuccessful = true;
         this.isSignUpFailed = false;
-        //this._router.navigate(['/subjects'])
+        // this._router.navigate(['/subjects'])
         // this.reloadPage();
         //this.toastr.success("Registration Was Successful")
         
