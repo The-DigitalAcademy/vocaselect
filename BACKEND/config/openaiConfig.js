39c122d5debfd4b3f3cb1e9fbd459111d52c2302
DAMAC194 <damac194@DA-MAC94s-MacBook-Air.local>
2023-08-01 15:58:55 +0200

    update

--- BACKEND/config/openaiConfig.js
+++ BACKEND/config/openaiConfig.js
@@ -2,7 +2,6 @@ const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 const configuration = new Configuration({
-  // apiKey: "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",
   apiKey: process.env.OpenAIApi
 });
 
