0e21806df8d704277d2383e50956932a379de44b
Khomotso Maincanye <khomomaincanye@gmail.com>
2023-08-04 14:50:23 +0200

    quiz and navbar complete

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -20,7 +20,9 @@ module.exports = {
     '2xl': '1536px',
     // => @media (min-width: 1536px) { ... }
   },
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [
     // require('@tailwindcss/typography'),
