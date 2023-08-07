61e3961586613ce7c15d14685b6516bf13141e39
DAMAC194 <damac194@DA-MAC94s-MacBook-Air.local>
2023-08-07 15:31:06 +0200

    updated

--- BACKEND/controllers/quizController.js
+++ BACKEND/controllers/quizController.js
@@ -11,18 +11,18 @@ exports.generateCareerQuiz = async (req, res) => {
     } = req.body;
 
     const prompt = `Quiz for Career Recommendation:\n
-    1. What are your favorite subjects or topics to learn about? (${answer1})\n
-    2. What do you enjoy doing in your free time? (${answer2})\n
-    3. How would you describe your personality? (e.g., outgoing, analytical, creative, etc.) (${answer3})\n
-    4. What are your long-term career goals? (${answer4})\n
-    5. Are you interested in remote work or prefer on-site positions? (${answer5})\n
-    6. Are you willing to undergo additional training or education to pursue a dream job? (${answer6})\n
-    7. Have you ever volunteered or had job previous? (${answer7})\n
-    8. What job roles or positions have you always been curious about? (${answer8})\n
-    9. Are you willing to invest extra hours for a job you're passionate about? (${answer9})\n
-    10. Is there any other information about yourself or your aspirations that you would like to share? (${answer10})\n
+    1. What's your favorite way to spend free time? (${answer1})\n
+    2. How would you describe your personality? (e.g., outgoing, analytical, creative, etc.) (${answer2})\n
+    3. Do you enjoy teamwork or individual work more? (${answer3})\n
+    4. What subjects or topics fascinate you? (${answer4})\n
+    5. Do you prefer a fast-paced and dynamic work environment or a more relaxed one? (${answer5})\n
+    6. Are you drawn to routine or unpredictability? (${answer6})\n
+    7. Are you looking for stability, flexibility, growth opportunities, or a mix of these in a job? (${answer7})\n
+    8. Would you rather master one skill or learn many? (${answer8})\n
+    9. How do you handle risk? (${answer9})\n
+    10. What's your dream way to spend a year off work? (${answer10})\n
 
-    Based on your answers, please recommend a maximum of six careers  and short interesting  description of what each profession does layman's terms to appeal to younger people(like you are explaining to a 5-year-old) in South Africa. Format the response JSON Format.`;
+    Based on your answers, please recommend a maximum of six (careers) careers and short interesting  description of what each profession does layman's terms to appeal to younger people(like you are explaining to a 5-year-old) in South Africa. Format the response JSON Format.`;
 
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
@@ -38,7 +38,7 @@ exports.generateCareerQuiz = async (req, res) => {
 
     // Filter out empty lines and display at least 4 suitable careers
     const suitableCareers = recommendedCareers.filter(career => career.trim() !== '');
-    const displayedCareers = suitableCareers.slice(0, Math.min(6, suitableCareers.length));
+    const displayedCareers = suitableCareers.slice(0, Math.min(16, suitableCareers.length)); 
 
     const jsonResult = { quizRecommendations: displayedCareers };
     res.status(200).json(jsonResult);
