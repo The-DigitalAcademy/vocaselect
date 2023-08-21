c38d1b231396db84628aec2d365f0b88b240e1ab
neo2621 <neomakhubo25@gmail.com>
2023-08-21 08:51:37 +0200

    timeline update

--- FRONTEND/src/app/Pages/timeline/timeline.component.ts
+++ FRONTEND/src/app/Pages/timeline/timeline.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TimelineService } from 'src/app/_services/_ChatGPT_Services/timeline.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-timeline',
@@ -10,10 +11,10 @@ import { Router } from '@angular/router';
 })
 export class TimelineComponent implements OnInit {
 
-  timeline :any = [];
+  timeline :any;
   grade: any;
-
-  constructor(private timelineService: TimelineService, private http:HttpClient, private router: Router){}  
+  user: any = {};
+  constructor(private timelineService: TimelineService, private http:HttpClient, private router: Router, private tokenStorage: TokenStorageService,){}  
 
   ngOnInit() {
     // Fetch items from the API when the component initializes
@@ -23,27 +24,27 @@ export class TimelineComponent implements OnInit {
 
   fetchTimeline() {
     // const apiUrl = 'YOUR_API_ENDPOINT'; // Replace with your actual API endpoint URL
-    // this.timelineService.getTimeline().subscribe(
-    //   (response) => {
-    //     this.timeline = response;
-    //   },
-    //   (error) => {
-    //     console.error('Error fetching items from the API:', error);
-    //   }
-    // );
+    this.timelineService.getTimeline().subscribe(
+      (response) => {
+        this.timeline = response;
+      },
+      (error) => {
+        console.error('Error fetching items from the API:', error);
+      }
+    );
     //need to get the grade from user data
-    this.grade = 11;
+    this.user = this.tokenStorage.getUser();  
 
-    const test = [{title: "Test 1", description: "testing description", grade:8}, 
-    {title: "Test 2", description: "testing description", grade:9},
-    {title: "Test 3", description: "testing description", grade:10},
-    {title: "Test 4", description: "testing description", grade:11},
-    {title: "Test 5", description: "testing description", grade:12},
-  ];
+  //   const test = [{title: "Test 1", description: "testing description", grade:8}, 
+  //   {title: "Test 2", description: "testing description", grade:9},
+  //   {title: "Test 3", description: "testing description", grade:10},
+  //   {title: "Test 4", description: "testing description", grade:11},
+  //   {title: "Test 5", description: "testing description", grade:12},
+  // ];
 
-  test.forEach((en:any)=>{
-    this.timeline.push(en)
-  });
-  }
+  // test.forEach((en:any)=>{
+  //   this.timeline.push(en)
+  // });
+   }
 
 }
