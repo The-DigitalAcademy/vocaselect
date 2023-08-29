156cc3cbc26253db4692cf143ed06ee9c810b766
enikma-mod <sirmodika@gmail.com>
2023-08-29 16:05:41 +0200

    swagger documentation update

--- BACKEND/routes/subjects.routes.js
+++ BACKEND/routes/subjects.routes.js
@@ -1,15 +1,69 @@
 const subjects = require('../controllers/Subjects/subjects.controllers')
   
-    const router = require("express").Router();
-  
-    router.post("/", subjects.createNewSubject);
+const router = require("express").Router();
 
 
-    //for getting all subjects
-    router.get("/", subjects.getAllsubjects);
+/**
+ * @swagger
+ * /api/subjects:
+ *   post:
+ *     summary: Create a new subject
+ *     tags: [Subjects]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/SubjectInput'
+ *     responses:
+ *       201:
+ *         description: Subject created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Subject'
+ *       409:
+ *         description: Details are not correct
+ *         content:
+ *           text/plain:
+ *             example: Details are not correct
+ */
 
-    // Create the route for deleting a subject
-    router.delete('/:id', subjects.deleteSubjectById);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     SubjectInput:
+ *       type: object
+ *       properties:
+ *         subjectName:
+ *           type: string
+ *         mark:
+ *           type: number
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Subject:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         subjectName:
+ *           type: string
+ *         mark:
+ *           type: number
+ */
+router.post("/", subjects.createNewSubject);
 
 
-    module.exports = router
+//for getting all subjects
+router.get("/", subjects.getAllsubjects);
+
+// Create the route for deleting a subject
+router.delete('/:id', subjects.deleteSubjectById);
+
+
+module.exports = router
