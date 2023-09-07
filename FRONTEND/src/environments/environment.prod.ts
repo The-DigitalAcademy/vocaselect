ddddd9f07aae78a2f8e77fbb25ce88cade2c8750
neo2621 <neomakhubo25@gmail.com>
2023-09-07 13:43:58 +0200

    subjects pages media query

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
- //baseUrl: 'http://localhost:9000/api/',
- baseUrl: 'https://vocaselect-backendfinal.onrender.com/api/',
+ baseUrl: 'http://localhost:9000/api/',
+//  baseUrl: 'https://vocaselect-backendfinal.onrender.com/api/',
 //  apiUrl: 'https://vocaselect-backendfinal.onrender.com',
 
  apiUrl: 'http://localhost:9000/',
